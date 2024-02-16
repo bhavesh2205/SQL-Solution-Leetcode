00f4394218e2cd641912936cea971dc116a582e1
Bhavesh Patil <114500492+bhavesh2205@users.noreply.github.com>
2024-02-16 14:02:13 +0530

    Create 577. Employee Bonus.sql

---
+++ Basic Joins/577. Employee Bonus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select Employee.name, Bonus.bonus
+
+from Employee 
+
+left join Bonus on Employee.empId = Bonus.empId
+
+where bonus < 1000 or bonus IS NULL;
