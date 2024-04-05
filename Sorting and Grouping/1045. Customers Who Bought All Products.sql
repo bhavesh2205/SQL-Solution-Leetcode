27d399fe3b810d3040a84319c31c9c7243020361
Bhavesh Patil <114500492+bhavesh2205@users.noreply.github.com>
2024-04-05 11:01:29 +0530

    Update 1045. Customers Who Bought All Products.sql

--- Sorting and Grouping/1045. Customers Who Bought All Products.sql
+++ Sorting and Grouping/1045. Customers Who Bought All Products.sql
@@ -1,1 +1,13 @@
+# Write your MySQL query statement below
+select customer_id 
 
+from Customer
+
+group by customer_id
+
+having count(distinct product_key) = (
+
+    select count(distinct product_key)
+
+    from Product
+);
